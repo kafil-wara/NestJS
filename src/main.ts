d3aa012fa4c47631038a5b4eb34486ad4e0d1bf3
kafil <18-38175-2@student.aiub.edu>
2023-04-11 07:37:18 +0600

    cors issue fixed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as session from 'express-session'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(
     session({
       secret: 'my-secret',
