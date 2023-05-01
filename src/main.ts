cab6bb608466464a1fc491e8d6cd03dc88016424
kafil <18-38175-2@student.aiub.edu>
2023-05-02 02:23:59 +0600

    cors..

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  app.enableCors();
 
   app.enableCors({
     allowedHeaders: '*',
