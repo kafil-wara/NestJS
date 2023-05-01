5cad7c95cfac7f32a4fbe995ee2eedb04d1925c1
kafil <18-38175-2@student.aiub.edu>
2023-05-02 01:31:58 +0600

    cors fixed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  app.use(cors({
+    origin: ['http://localhost:3000', 'https://nextjs-zc9awgc1e-kafil-wara.vercel.app/'],
+    credentials: true, 
+  }));
 
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
