a412be2f12c9c7c951fdce12b2f5e11a0a2eeb83
kafil <18-38175-2@student.aiub.edu>
2023-05-02 02:16:44 +0600

    fk cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
-  app.use(cors({
-    origin: ['http://localhost:3000', 'https://nextjs-six-iota-11.vercel.app/'],
-    credentials: true, 
-  }));
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
 
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
